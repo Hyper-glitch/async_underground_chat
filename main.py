56e15ae31b50139e8286834054b8fad36e35af12
hyper-glitch <graff049@gmail.com>
2022-08-17 19:34:00 +0300

    feature(many): 1. create async tasks 2. rename args

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import argparse
 import asyncio
-import datetime
-import time
 
-import aiofiles
-
-from settings import RECONNECTION_WAIT_TIME, CHAT_PORT, CHAT_HOST, CHAT_HISTORY_PATH
+from reader import read_messages
+from sender import send_messages
+from settings import READ_CHAT_PORT, CHAT_HOST, CHAT_HISTORY_PATH, SEND_CHAT_PORT
 
 
 def create_parser() -> argparse.Namespace:
@@ -19,63 +17,34 @@ def create_parser() -> argparse.Namespace:
         '-H', '--host', help='TCP/IP hostname to serve on (default: %(default)r)',
     )
     parser.add_argument(
-        '-P', '--port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
+        '-R', '--read_port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
     )
     parser.add_argument(
-        '-p', '--path', help='path to write chat history', type=str,
+        '-S', '--send_port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
+    )
+    parser.add_argument(
+        '-P', '--path', help='path to write chat history', type=str,
     )
     return parser.parse_args()
 
 
-async def write_chat_msg(data: str, path: str):
-    """Write messages from chat line by line to a file.
-
-    Args:
-        data: message from chat for writing in a file.
-        path: filepath for chat history.
-    """
-    async with aiofiles.open(path, mode='a') as file:
-        await file.write(data)
-
-
-async def listen_chat(host: str, port: int, path: str):
-    """Write messages from chat line by line to a file.
-
-    Args:
-        host: TCP/IP hostname to open listen connection.
-        port: TCP/IP port to open listen connection.
-        path: filepath for chat history.
-
-    Raises:
-        Exception: if connection between server and client lost.
-    """
-    while True:
-        try:
-            reader, writer = await asyncio.open_connection(host=host, port=port)
-        except Exception:
-            time.sleep(RECONNECTION_WAIT_TIME)
-            continue
-
-        message = await reader.readline()
-
-        now = datetime.datetime.now()
-        formatted_date = now.strftime("%Y.%m.%d %H:%M:%S")
-
-        formatted_message = f'[{formatted_date}] {message.decode()}'
-        print(formatted_message)
-        await write_chat_msg(data=formatted_message, path=path)
-
-
-def main():
+async def main():
     """Run the main logic for chat."""
     args = create_parser()
 
     host = CHAT_HOST if not args.host else args.host
-    port = CHAT_PORT if not args.port else args.port
+    read_port = READ_CHAT_PORT if not args.read_port else args.read_port
+    send_port = SEND_CHAT_PORT if not args.send_port else args.send_port
     path = CHAT_HISTORY_PATH if not args.path else args.path
+    token = '66b440d2-1e24-11ed-8c47-0242ac110002\n'
 
-    asyncio.run(listen_chat(host=host, port=port, path=path))
+    tasks = [
+        asyncio.create_task(read_messages(host=host, port=read_port, path=path)),
+        asyncio.create_task(send_messages(host=host, port=send_port, token=token)),
+    ]
+
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
