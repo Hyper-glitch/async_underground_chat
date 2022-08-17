b9eed911f4869d571fddd392b308e73d1c46503c
hyper-glitch <graff049@gmail.com>
2022-08-17 19:31:50 +0300

    feature(constants): add and rename constants

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ env = Env()
 env.read_env()
 
 CHAT_HOST = env.str('HOST', 'minechat.dvmn.org')
-CHAT_PORT = env.int('PORT', 5000)
+READ_CHAT_PORT = env.int('PORT', 5000)
+SEND_CHAT_PORT = env.int('PORT', 5050)
 CHAT_HISTORY_PATH = env.str('HISTORY_PATH', 'minechat.history')
 
 RECONNECTION_WAIT_TIME = 180
