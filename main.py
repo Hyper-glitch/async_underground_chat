4f1e04e2769bdf9d77a2d8a7c2b92186ce91c8d8
hyper-glitch <graff049@gmail.com>
2022-08-17 14:02:33 +0300

    refactor(env, args): add virtual vars and also cli args

--- main.py
+++ main.py
@@ -15,27 +15,26 @@ def create_parser() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-H', '--host', help='TCP/IP hostname to serve on (default: %(default)r)', default='minechat.dvmn.org',
+        '-H', '--host', help='TCP/IP hostname to serve on (default: %(default)r)',
     )
     parser.add_argument(
-        '-P', '--port', help='TCP/IP port to serve on (default: %(default)r)', type=int, default='5000',
+        '-P', '--port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
     )
     parser.add_argument(
-        '-p', '--path', help='path to write chat history', type=str, default='minechat.history',
+        '-p', '--path', help='path to write chat history', type=str,
     )
     return parser.parse_args()
 
 
-async def write_chat_msg(data):
-    async with aiofiles.open(CHAT_HISTORY_PATH, mode='a') as file:
+async def write_chat_msg(data, path):
+    async with aiofiles.open(path, mode='a') as file:
         await file.write(data)
 
 
-async def listen_chat():
-
+async def listen_chat(host, port, path):
     while True:
         try:
-            reader, writer = await asyncio.open_connection(host=CHAT_HOST, port=CHAT_PORT)
+            reader, writer = await asyncio.open_connection(host=host, port=port)
         except Exception:
             time.sleep(RECONNECTION_WAIT_TIME)
             continue
@@ -46,9 +45,19 @@ async def listen_chat():
         formatted_date = now.strftime("%Y.%m.%d %H:%M:%S")
 
         formatted_message = f'[{formatted_date}] {message.decode()}'
-        await write_chat_msg(data=formatted_message)
         print(formatted_message)
+        await write_chat_msg(data=formatted_message, path=path)
+
+
+def main():
+    args = create_parser()
+
+    host = CHAT_HOST if not args.host else args.host
+    port = CHAT_PORT if not args.port else args.port
+    path = CHAT_HISTORY_PATH if not args.path else args.path
+
+    asyncio.run(listen_chat(host=host, port=port, path=path))
 
 
 if __name__ == '__main__':
-    asyncio.run(listen_chat())
+    main()
