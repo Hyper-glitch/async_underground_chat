9b5824a9ffa8fae10a7cf7eea371428bfc79f213
hyper-glitch <graff049@gmail.com>
2022-08-18 13:16:22 +0300

    feature(write): write data to a file depends on data type

--- chat_utils.py
+++ chat_utils.py
@@ -1,12 +1,18 @@
+import json
+from typing import Union
+
 import aiofiles
 
 
-async def write_chat_msg(data: str, path: str):
+async def write_to_file(data: Union[str, dict], path: str):
     """Write messages from chat line by line to a file.
 
     Args:
         data: message from chat for writing in a file.
         path: filepath for chat history.
     """
+    if isinstance(data, dict):
+        data = json.dumps(data)
+
     async with aiofiles.open(path, mode='a') as file:
         await file.write(data)
