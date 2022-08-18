28d3fe12d4c00c447eec51de960e049d306ffabe
hyper-glitch <graff049@gmail.com>
2022-08-18 15:14:40 +0300

    refactor(code): make refactoring greate again

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import asyncio
 import logging
 
-from chat_utils import create_parser
-from reader import read_messages
-from settings import READ_CHAT_PORT, CHAT_HOST, CHAT_HISTORY_PATH
+from reader import run_reader
+from sender import run_sender
 
 logger = logging.getLogger(__name__)
 
 
 async def main():
     """Run the main logic for chat."""
-    args = create_parser()
-
     logging.basicConfig(
         format=u'%(levelname)s %(filename)s %(message)s', level=logging.DEBUG,
     )
-
-    host = CHAT_HOST if not args.host else args.host
-    read_port = READ_CHAT_PORT if not args.read_port else args.read_port
-    path = CHAT_HISTORY_PATH if not args.path else args.path
-
     tasks = [
-        asyncio.create_task(read_messages(host=host, port=read_port, path=path)),
+        asyncio.create_task(run_reader()),
+        asyncio.create_task(run_sender()),
     ]
-
     await asyncio.gather(*tasks)
 
 
