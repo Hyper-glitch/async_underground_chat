5d61f929d89bed15a12c0d2080715286fcb7bf7f
hyper-glitch <graff049@gmail.com>
2022-08-18 15:04:41 +0300

    refactor(move): move funcs to utils

--- chat_utils.py
+++ chat_utils.py
@@ -1,7 +1,18 @@
+import argparse
 import json
 from typing import Union
 
 import aiofiles
+
+
+async def read_line(reader) -> str:
+    chat_line = await reader.readline()
+    return chat_line.decode('utf-8').strip()
+
+
+async def write_data(writer, data: str):
+    writer.write(data.encode())
+    await writer.drain()
 
 
 async def write_to_file(data: Union[str, dict], path: str):
@@ -16,3 +27,34 @@ async def write_to_file(data: Union[str, dict], path: str):
 
     async with aiofiles.open(path, mode='a') as file:
         await file.write(data)
+
+
+def create_parser() -> argparse.Namespace:
+    """Create arg parser and add arguments.
+
+    Returns:
+        namespace: values or arguments, that parsed.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-H', '--host', help='TCP/IP hostname to serve on (default: %(default)r)',
+    )
+    parser.add_argument(
+        '-R', '--read_port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
+    )
+    parser.add_argument(
+        '-S', '--send_port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
+    )
+    parser.add_argument(
+        '-P', '--path', help='path to write chat history', type=str,
+    )
+    parser.add_argument(
+        '-T', '--token', help='token that authorize user in chat', type=str,
+    )
+    parser.add_argument(
+        '-U', '--username', help='your alias in chat', type=str,
+    )
+    parser.add_argument(
+        '-M', '--message', help='message that sends to the chat', type=str, nargs='+', required=True,
+    )
+    return parser.parse_args()
