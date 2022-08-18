7ee5f65d7543d653ded759608522fad917f2d2ed
hyper-glitch <graff049@gmail.com>
2022-08-18 10:50:28 +0300

    feature(token): add auth token var

--- settings.py
+++ settings.py
@@ -8,5 +8,6 @@ CHAT_HOST = env.str('HOST', 'minechat.dvmn.org')
 READ_CHAT_PORT = env.int('PORT', 5000)
 SEND_CHAT_PORT = env.int('PORT', 5050)
 CHAT_HISTORY_PATH = env.str('HISTORY_PATH', 'minechat.history')
+AUTH_TOKEN = env.str('AUTH_TOKEN', '')
 
 RECONNECTION_WAIT_TIME = 180
