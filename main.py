a8ef9e56c5b7c9789c14acb63007d5b887c75036
hyper-glitch <graff049@gmail.com>
2022-08-18 15:04:22 +0300

    feature(delete): remove extra code

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-import argparse
 import asyncio
 import logging
 
+from chat_utils import create_parser
 from reader import read_messages
-from sender import registrate
-from settings import READ_CHAT_PORT, CHAT_HOST, CHAT_HISTORY_PATH, SEND_CHAT_PORT, AUTH_TOKEN
+from settings import READ_CHAT_PORT, CHAT_HOST, CHAT_HISTORY_PATH
 
 logger = logging.getLogger(__name__)
-
-
-def create_parser() -> argparse.Namespace:
-    """Create arg parser and add arguments.
-
-    Returns:
-        namespace: values or arguments, that parsed.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-H', '--host', help='TCP/IP hostname to serve on (default: %(default)r)',
-    )
-    parser.add_argument(
-        '-R', '--read_port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
-    )
-    parser.add_argument(
-        '-S', '--send_port', help='TCP/IP port to serve on (default: %(default)r)', type=int,
-    )
-    parser.add_argument(
-        '-P', '--path', help='path to write chat history', type=str,
-    )
-    return parser.parse_args()
 
 
 async def main():
@@ -41,14 +18,10 @@ async def main():
 
     host = CHAT_HOST if not args.host else args.host
     read_port = READ_CHAT_PORT if not args.read_port else args.read_port
-    send_port = SEND_CHAT_PORT if not args.send_port else args.send_port
     path = CHAT_HISTORY_PATH if not args.path else args.path
-    token = AUTH_TOKEN
-    nickname = 'jepka'
 
     tasks = [
         asyncio.create_task(read_messages(host=host, port=read_port, path=path)),
-        asyncio.create_task(registrate(host=host, port=send_port, nickname=nickname)),
     ]
 
     await asyncio.gather(*tasks)
