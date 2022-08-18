78f238b3dd4ab5b62ee3a462f393e582f0a960b7
hyper-glitch <graff049@gmail.com>
2022-08-18 15:03:38 +0300

    feature(rename): rename constant

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ READ_CHAT_PORT = env.int('PORT', 5000)
 SEND_CHAT_PORT = env.int('PORT', 5050)
 CHAT_HISTORY_PATH = env.str('HISTORY_PATH', 'minechat.history')
 AUTH_TOKEN = env.str('AUTH_TOKEN', '')
+NICKNAME = env.str('NICKNAME', 'anon')
 
 FAILED_AUTH_MESSAGE = 'Неизвестный токен. Проверьте его или зарегистрируйтесь заново.'
 RECONNECTION_WAIT_TIME = 180
