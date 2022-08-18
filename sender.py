f116b442d8a45b7f6d6d845363576bb8a4aae519
hyper-glitch <graff049@gmail.com>
2022-08-18 12:33:35 +0300

    feature(auth): Add check for broken token.

--- sender.py
+++ sender.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import logging
 
-from settings import CHAT_HOST, SEND_CHAT_PORT, AUTH_TOKEN
+from settings import CHAT_HOST, SEND_CHAT_PORT, AUTH_TOKEN, FAILED_AUTH_MESSAGE
 
 logger = logging.getLogger(__name__)
 
@@ -12,9 +12,22 @@ async def read_line(reader) -> str:
     return chat_line.decode('utf-8').strip()
 
 
-async def write_data(writer, data):
+async def write_data(writer, data: str):
     writer.write(data.encode())
     await writer.drain()
+
+
+async def auth(host, port, token: str):
+    reader, writer = await asyncio.open_connection(host=host, port=port)
+
+    logger.debug(await read_line(reader))
+    await write_data(writer=writer, data=f'{token}\n')
+
+    account_info = json.loads(await read_line(reader))
+    logger.debug(account_info)
+
+    assert account_info is not None, FAILED_AUTH_MESSAGE
+    return account_info
 
 
 async def send_messages(host: str, port: int, token: str, message: str):
@@ -35,4 +48,5 @@ if __name__ == '__main__':
     host = CHAT_HOST
     send_port = SEND_CHAT_PORT
     token = AUTH_TOKEN
-    asyncio.run(send_messages(host=host, port=send_port, token=token, message='blablabla'))
+
+    asyncio.run(auth(token=token))
