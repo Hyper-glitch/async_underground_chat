5b74162f42211c56c0b7ed651c4306ac0816075e
hyper-glitch <graff049@gmail.com>
2022-08-18 10:50:08 +0300

    feature(logging): set up logging

--- sender.py
+++ sender.py
@@ -1,12 +1,15 @@
 import asyncio
 import json
+import logging
 
-from settings import CHAT_HOST, SEND_CHAT_PORT
+from settings import CHAT_HOST, SEND_CHAT_PORT, AUTH_TOKEN
+
+logger = logging.getLogger(__name__)
 
 
 async def read_line(reader) -> str:
-    chat_line = (await asyncio.wait_for(reader.readline(), timeout=10))
-    return chat_line.decode("utf-8").strip()
+    chat_line = await reader.readline()
+    return chat_line.decode('utf-8').strip()
 
 
 async def write_data(writer, data):
@@ -14,19 +17,22 @@ async def write_data(writer, data):
     await writer.drain()
 
 
-async def send_messages(host: str, port: int, token):
-    test_message = 'Something else\n\n'
+async def send_messages(host: str, port: int, token: str, message: str):
     reader, writer = await asyncio.open_connection(host=host, port=port)
+    logger.debug(await read_line(reader))
 
-    await write_data(writer, data=token)
-    account_info = json.loads(await read_line(reader))
+    await write_data(writer, data=f'{token}\n')
+    account_info = json.loads(await reader.readline())
+    logger.debug(await read_line(reader))
 
-    await write_data(writer, data=test_message)
+    await write_data(writer, data=f'{message}\n\n')
+    logger.debug(await read_line(reader))
+
     writer.close()
 
 
 if __name__ == '__main__':
     host = CHAT_HOST
     send_port = SEND_CHAT_PORT
-
-    asyncio.run(send_messages(host=host, port=send_port, token=token))
+    token = AUTH_TOKEN
+    asyncio.run(send_messages(host=host, port=send_port, token=token, message='blablabla'))
