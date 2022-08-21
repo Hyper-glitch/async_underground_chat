eddad714f47e3b891170ad9b4dfba1e4b2e22300
hyper-glitch <graff049@gmail.com>
2022-08-21 23:01:09 +0300

    refactor(only one connection): open just one connection and one time file

--- reader.py
+++ reader.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
 
-from chat_utils import write_to_file, create_parser, open_connection
+import aiofiles
+
+from chat_utils import create_parser, open_connection
 from settings import CHAT_HOST, READ_CHAT_PORT, CHAT_HISTORY_PATH
 
 logger = logging.getLogger(__name__)
@@ -19,14 +21,15 @@ async def run_reader():
     read_port = READ_CHAT_PORT if not args.read_port else args.read_port
     path = CHAT_HISTORY_PATH if not args.path else args.path
 
-    while True:
+    async with aiofiles.open(path, mode='a') as file:
         async with open_connection(host, read_port) as conn:
             reader, _ = conn
-            message = await reader.readline()
 
-        now = datetime.datetime.now()
-        formatted_date = now.strftime("%Y.%m.%d %H:%M:%S")
+            while True:
+                message = await reader.readline()
+                now = datetime.datetime.now()
+                formatted_date = now.strftime("%Y.%m.%d %H:%M:%S")
 
-        formatted_message = f'[{formatted_date}] {message.decode()}'
-        print(formatted_message)
-        await write_to_file(data=formatted_message, path=path)
+                formatted_message = f'[{formatted_date}] {message.decode()}'
+                print(formatted_message)
+                await file.write(formatted_message)
