64f7d27904d7b80c0288094bc313efada1c6497f
hyper-glitch <graff049@gmail.com>
2022-08-24 13:53:27 +0300

    feature(sender): send new msgs to a chat

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import asyncio
 import aiofiles
 
 import gui
-from chat_utils import create_parser
 from reader import read_msgs
-from settings import CHAT_HOST, READ_CHAT_PORT, CHAT_HISTORY_PATH
+from sender import send_msgs
+from settings import CHAT_HISTORY_PATH
 
 
 async def show_history(messages_queue):
@@ -16,23 +16,15 @@ async def show_history(messages_queue):
 
 
 async def main():
-    parser = create_parser()
-    args = parser.parse_args()
-
-    host = args.host or CHAT_HOST
-    read_port = args.read_port or READ_CHAT_PORT
-    path = args.path or CHAT_HISTORY_PATH
-
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
-    reader_args = [messages_queue, host, read_port, path]
-
     await asyncio.gather(
-        read_msgs(*reader_args),
-        show_history(messages_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue),
+        read_msgs(messages_queue),
+        show_history(messages_queue),
+        send_msgs(sending_queue),
     )
 
 
