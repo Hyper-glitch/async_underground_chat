137c261545bee99ce6685cbe6f15e8514ca8ada0
hyper-glitch <graff049@gmail.com>
2022-08-24 12:07:38 +0300

    feature(reader args): parse host, port, path from cli or env

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import asyncio
+
 import gui
+from chat_utils import create_parser
+from reader import read_msgs
+from settings import CHAT_HOST, READ_CHAT_PORT, CHAT_HISTORY_PATH
 
-loop = asyncio.get_event_loop()
 
-messages_queue = asyncio.Queue()
-sending_queue = asyncio.Queue()
-status_updates_queue = asyncio.Queue()
+async def main():
+    parser = create_parser()
+    args = parser.parse_args()
 
-loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+    host = args.host or CHAT_HOST
+    read_port = args.read_port or READ_CHAT_PORT
+    path = args.path or CHAT_HISTORY_PATH
+
+    messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+
+    reader_args = [messages_queue, host, read_port, path]
+
+    await asyncio.gather(
+        gui.draw(messages_queue, sending_queue, status_updates_queue),
+        read_msgs(*reader_args),
+    )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
