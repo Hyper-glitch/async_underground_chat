744e19d4c4de4bfc95042a78b486d7e4bca8746b
hyper-glitch <graff049@gmail.com>
2022-08-24 13:54:19 +0300

    refactor(arg_parser): parse args from reader

--- reader.py
+++ reader.py
@@ -2,15 +2,23 @@ import datetime
 
 import aiofiles
 
-from chat_utils import open_connection
+from chat_utils import open_connection, create_parser
+from settings import CHAT_HOST, READ_CHAT_PORT, CHAT_HISTORY_PATH
 
 
-async def read_msgs(messages_queue, host, read_port, path):
+async def read_msgs(messages_queue):
     """Write messages from chat line by line to a file.
 
     Raises:
         Exception: if connection between server and client lost.
     """
+    parser = create_parser()
+    args = parser.parse_args()
+
+    host = args.host or CHAT_HOST
+    read_port = args.read_port or READ_CHAT_PORT
+    path = args.path or CHAT_HISTORY_PATH
+
     async with aiofiles.open(path, mode='a') as file:
         async with open_connection(host, read_port) as conn:
             reader, _ = conn
