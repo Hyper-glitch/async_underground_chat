69d01d125fc0da20ea8602cac7f9c0a01a426eb7
hyper-glitch <graff049@gmail.com>
2022-08-24 10:50:04 +0300

    feature(consts): add constant vars for gui chat

--- settings.py
+++ settings.py
@@ -10,6 +10,8 @@ SEND_CHAT_PORT = env.int('SEND_PORT', 5050)
 CHAT_HISTORY_PATH = env.str('HISTORY_PATH', 'minechat.history')
 AUTH_TOKEN = env.str('AUTH_TOKEN', '')
 NICKNAME = env.str('NICKNAME', 'anon')
+GUI_TITTLE = 'Чат Майнкрафтера'
+SEND_GUI_BUTTON = 'Отправить'
 
 FAILED_AUTH_MESSAGE = 'Неизвестный токен. Проверьте его или зарегистрируйтесь заново.'
 RECONNECTION_WAIT_TIME = 180
