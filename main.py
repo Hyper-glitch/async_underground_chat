2e050048d5b93b00d51bd7bbb2242d43d91aacaa
hyper-glitch <graff049@gmail.com>
2022-08-25 18:15:36 +0300

    feature(create_task_group): use context manager instead of asyncio.gather

--- main.py
+++ main.py
@@ -15,14 +15,6 @@ from settings import LAST_GUI_DRAW_QUEUE, TIMEOUT_ERROR_TEXT
 watchdog_logger = logging.getLogger('watchdog_logger')
 
 
-async def start_task_group(messages_queue, status_updates_queue, watchdog_queue, sending_queue):
-    async with create_task_group() as tg:
-        await tg.start(ping_server, watchdog_queue)
-        await tg.start(read_msgs, messages_queue, status_updates_queue, watchdog_queue)
-        await tg.start(send_msgs, sending_queue, status_updates_queue, watchdog_queue)
-        await tg.start(watch_for_connection, watchdog_queue)
-
-
 async def watch_for_connection(watchdog_queue, task_status: TaskStatus = TASK_STATUS_IGNORED):
     task_status.started()
 
@@ -34,10 +26,23 @@ async def watch_for_connection(watchdog_queue, task_status: TaskStatus = TASK_ST
         watchdog_logger.info(log)
 
 
-async def handle_connection(messages_queue, sending_queue, status_updates_queue, watchdog_queue):
+async def start_server_task_group(messages_queue, status_updates_queue, watchdog_queue, sending_queue):
+    async with create_task_group() as tg:
+        await tg.start(ping_server, watchdog_queue)
+        await tg.start(read_msgs, messages_queue, status_updates_queue, watchdog_queue)
+        await tg.start(send_msgs, sending_queue, status_updates_queue, watchdog_queue)
+        await tg.start(watch_for_connection, watchdog_queue)
+
+
+async def handle_connection(
+        messages_queue, sending_queue, status_updates_queue,
+        watchdog_queue, task_status: TaskStatus = TASK_STATUS_IGNORED
+):
+    task_status.started()
+
     while True:
         try:
-            await start_task_group(messages_queue, status_updates_queue, watchdog_queue, sending_queue)
+            await start_server_task_group(messages_queue, status_updates_queue, watchdog_queue, sending_queue)
         except (ConnectionError, gaierror):
             status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
             status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.CLOSED)
@@ -56,11 +61,10 @@ async def main():
 
     set_up_logger()
 
-    await asyncio.gather(
-        gui.draw(*queues[:LAST_GUI_DRAW_QUEUE]),
-        show_history(messages_queue),
-        handle_connection(*queues),
-    )
+    async with create_task_group() as tg:
+        await tg.start(gui.draw, *queues[:LAST_GUI_DRAW_QUEUE])
+        await tg.start(show_history, messages_queue)
+        await tg.start(handle_connection, *queues)
 
 
 if __name__ == '__main__':
